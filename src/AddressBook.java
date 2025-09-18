01e9b6b15cdd6361e1a5faee59371434ccd90129
Kevin Abeykoon <abeykoonkevin@gmail.com>
2025-09-18 10:22:04 -0400

    changed output string

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,5 +1,12 @@
 public class AddressBook {
+    int x;
+    public AddressBook(){
+        x = 1;
+    }
+
     public static void main(String[] args){
+        BuddyInfo obj = new BuddyInfo("Tom", "Sun St.", 1031213);
+        AddressBook book = new AddressBook();
         System.out.println("Address Book changed");
 
     }
